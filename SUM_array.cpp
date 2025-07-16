16d239d4cf064b82baf92913cc9dac716f9026d6
Maru Dhanvi <marudhanvi8@gmail.com>
2025-07-16 23:28:15 +0530

    Update and rename all (4).cpp to SUM_array.cpp

--- SUM_array.cpp
+++ SUM_array.cpp
@@ -1,13 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-	// your code goes here
+	
 //finding sum of all the elements in array 
 
 int arr[10],sum=0;
 cout<<"enter numberss:"<<endl;
-for(int i=1;i<=10;i++)
+for(int i=1; i<=10; i++)
 {
     cin>>i;
     sum = sum+=i;
